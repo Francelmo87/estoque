5169736c2132015cdaa52a15928cc6acaa7def8e
Francelmo <francelmo87@hotmail.com>
2025-02-13 22:55:28 -0300

    Criada a categoria e produtos no admin

--- api/produto/admin.py
+++ api/produto/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Categoria, Produto
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'categoria')
+    search_fields = ('categoria',)
+   
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'produto','unidade','estoque','estoque_minimo','preco','data',)
+    search_fields = ('produto',)
+   
