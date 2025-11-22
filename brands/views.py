6b9f8b3405068593a5845a55235cf363e4843554
Francelmo87 <francelmo87@hotmail.com>
2025-11-22 14:08:22 -0300

    created listview

--- brands/views.py
+++ brands/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Brand
+
+class BrandListView(ListView):
+    model = Brand
+    template_name = 'brand_list.html'
+    context_object_name = 'brands'
+        
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        name = self.request.GET.get('name')
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
