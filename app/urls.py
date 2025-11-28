a691f7cf6d4c8b7cc677dbf3e02391fd49b809d8
Francelmo87 <francelmo87@hotmail.com>
2025-11-27 21:47:43 -0300

    created all urls of products

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('brands/', include('brands.urls')),
     path('categories/', include('categories.urls')),
     path('suppliers/', include('suppliers.urls')),
+    path('products/', include('products.urls')),
 ]
