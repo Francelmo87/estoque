1129a2d315cbe94fcca9178d67908ea9ac9ff6b3
Francelmo87 <francelmo87@hotmail.com>
2025-11-27 22:01:09 -0300

    created all urls of products correctly

--- products/urls.py
+++ products/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path('products/list/', views.ProductListView.as_view(), name='product_list'),
-    path('products/create/', views.ProductCreateView.as_view(), name='product_create'),
-    path('products/<int:pk>/detail/', views.ProductDetailView.as_view(), name='product_detail'),
-    path('products/<int:pk>/update/', views.ProductUpdateView.as_view(), name='product_update'),
-    path('products/<int:pk>/delete/', views.ProductDeleteView.as_view(), name='product_delete'),
+    path('list/', views.ProductListView.as_view(), name='product_list'),
+    path('create/', views.ProductCreateView.as_view(), name='product_create'),
+    path('<int:pk>/detail/', views.ProductDetailView.as_view(), name='product_detail'),
+    path('<int:pk>/update/', views.ProductUpdateView.as_view(), name='product_update'),
+    path('<int:pk>/delete/', views.ProductDeleteView.as_view(), name='product_delete'),
 ]
    
