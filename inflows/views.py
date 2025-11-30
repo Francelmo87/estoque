5efb3f7c6c6441917edd981eb163851fe10c01fd
Francelmo87 <francelmo87@hotmail.com>
2025-11-29 21:16:34 -0300

    created the pagination of inflows

--- inflows/views.py
+++ inflows/views.py
@@ -9,6 +9,7 @@ class InflowListView(ListView):
     model = Inflow
     template_name = 'inflow_list.html'
     context_object_name = 'inflows'
+    paginate_by = 10
    
     def get_queryset(self):
         queryset = super().get_queryset()
